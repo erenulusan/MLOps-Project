54f7423a27c35f2c26ef30b1513f89028e81fc21
Eren Ulusan <erenulusann@gmail.com>
2025-06-17 01:24:30 +0300

    req added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="eren",
+    author_email="erenulusann@gmail.com",
+    packages=find_packages())
